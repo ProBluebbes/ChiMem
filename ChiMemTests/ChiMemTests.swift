12e28f918d8ccc4316b644ad38e6d9c77c6cef08
Stanislav Zhelokhovtsev <sz7502@princeton.edu>
2026-06-23 20:01:34 -0700

    Initial Commit

---
+++ ChiMemTests/ChiMemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChiMemTests.swift
+//  ChiMemTests
+//
+//  Created by stan on 6/23/26.
+//
+
+import Testing
+@testable import ChiMem
+
+struct ChiMemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
